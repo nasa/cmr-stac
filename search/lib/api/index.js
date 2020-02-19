4c19ee1d92937927a6be12596dd4f1e3b8ecc349
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2020-02-19 12:50:19 -0500

    CMR-6214: Add search health dependency.

--- search/lib/api/index.js
+++ search/lib/api/index.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
 const path = require('path');
 const express = require('express');
+const cmr = require('../cmr');
 
 const { createRedirectUrl, WfsLink, generateAppUrl, logger } = require('../util');
 
@@ -17,10 +19,28 @@ function createRootResponse (event) {
   };
 }
 
+async function getHealth (res) {
+  try {
+    await axios.get(cmr.makeCmrSearchUrl('/health'));
+    res.status(200).json({
+      search: {
+        'ok?': true
+      }
+    });
+  } catch (error) {
+    res.status(503).json({
+      search: {
+        'ok?': false
+      }
+    });
+  }
+}
+
 const routes = express.Router();
 
 routes.use(stac.routes);
 routes.use(wfs.routes);
+routes.use('/health', (req, res) => getHealth(res));
 routes.use('/docs', express.static(path.join(__dirname, '../../docs')));
 routes.use('/', (req, res) =>
   req.accepts('html') === 'html' ? res.redirect(createRedirectUrl(req, `/docs/index.html`)) : res.status(200).json(createRootResponse(req.apiGateway.event)));
