7d80bfb884d1dbf4d5f10da09fc5fbb009792dfe
Drew Daniel <drew@element84.com>
2020-06-09 15:13:27 -0400

    added tests around getProviders. Refactored code

--- search/tests/api/provider.spec.js
+++ search/tests/api/provider.spec.js
@@ -1,9 +1,24 @@
 const { getProviders } = require('../../lib/api/provider');
 
 describe('getProviders', () => {
-  it.skip('should return an array', async () => {
-    const providerList = await getProviders();
-    console.log(providerList);
-    expect(providerList).toBe('true');
+  const testEvent = {
+    headers: {
+      host: 'http://example.com'
+    }
+  };
+
+  it('should require an event parameter', async () => {
+    expect(() => getProviders().resolve()).toThrow();
+  });
+
+  it('should return an array', async () => {
+    const providerList = await getProviders(testEvent);
+    expect(Array.isArray(providerList)).toEqual(true);
+  });
+
+  it('should return an array containing at least one object', async () => {
+    const providerList = await getProviders(testEvent);
+    expect(providerList.length).toBeGreaterThan(0);
+    expect(typeof providerList[0]).toBe('object');
   });
 });
