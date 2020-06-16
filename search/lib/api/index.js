3770da57d68e6e442ba2a24932f66cd09f6c8bcd
Drew Daniel <drew@element84.com>
2020-06-16 17:43:07 -0400

    Removed comments and refactored for readability

--- search/lib/api/index.js
+++ search/lib/api/index.js
@@ -2,24 +2,10 @@ const axios = require('axios');
 const path = require('path');
 const express = require('express');
 const cmr = require('../cmr');
-const provider = require('./provider');
 
+const provider = require('./provider');
 const stac = require('./stac');
 const wfs = require('./wfs');
-
-// async function getProvidersRoot (req, res) {
-//   try {
-//     const providerObjects = await provider.getProviders(req);
-//     const providerCatalog = {
-//       id: 'cmr-stac',
-//       description: 'This is the landing page for CMR-STAC. Each provider link below contains a STAC endpoint.',
-//       links: providerObjects
-//     };
-//     res.status(200).json(providerCatalog);
-//   } catch (error) {
-//     res.status(400).error(error);
-//   }
-// }
 
 async function getHealth (res) {
   try {
@@ -45,7 +31,6 @@ routes.use(stac.routes);
 routes.use(wfs.routes);
 routes.use('/health', (req, res) => getHealth(res));
 routes.use('/docs', express.static(path.join(__dirname, '../../docs')));
-// routes.use('/', (req, res) => getProvidersRoot(req, res));
 module.exports = {
   routes
 };
