1fbd1d61e2f0d6bb2edd444a6339fc90bb2e29aa
Drew Daniel <drew@element84.com>
2020-07-01 09:36:50 -0400

    tixed stac query mapping issue

--- search/lib/cmr.js
+++ search/lib/cmr.js
@@ -16,7 +16,7 @@ const STAC_SEARCH_PARAMS_CONVERSION_MAP = {
 const STAC_QUERY_PARAMS_CONVERSION_MAP = {
   limit: ['limit', (v) => parseInt(v, 10)],
   bbox: ['bbox', parseOrdinateString],
-  datetime: ['time', identity]
+  datetime: ['temporal', identity]
 };
 
 const WFS_PARAMS_CONVERSION_MAP = {
