03694319b0e30b5a2023413206f550453d3956ca
Matthew Hanson <matt.a.hanson@gmail.com>
2021-04-28 01:29:27 -0400

    add additional conformance classes

--- search/lib/api/provider.js
+++ search/lib/api/provider.js
@@ -60,6 +60,10 @@ async function getProvider (request, response) {
       conformsTo: [
         'https://api.stacspec.org/v1.0.0-beta.1/core',
         'https://api.stacspec.org/v1.0.0-beta.1/item-search',
+        'https://api.stacspec.org/v1.0.0-beta.1/item-search#fields',
+        'https://api.stacspec.org/v1.0.0-beta.1/item-search#query',
+        'https://api.stacspec.org/v1.0.0-beta.1/item-search#sort',
+        'https://api.stacspec.org/v1.0.0-beta.1/item-search#context',
         'http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core',
         'http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/oas30',
         'http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/geojson'
