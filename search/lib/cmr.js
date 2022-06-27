5e377e51e186b0dea0a479103f9bfc3b10f96759
Jay Barra <jay.f.barra@gmail.com>
2022-06-27 18:35:09 -0400

    CMR-8320 removed debugs

--- search/lib/cmr.js
+++ search/lib/cmr.js
@@ -64,7 +64,7 @@ async function cmrSearch (path, params) {
 
   // STAC paging starts at 1
   const pageNum = Number.isNaN(Number(params['page_num'], 10))
-        ? 1 : Number(params['page_num'], 10);
+    ? 1 : Number(params['page_num'], 10);
 
   // Check if a search-after is available from the prior search
   const searchAfter = searchAfterCache.get(quickHash(`${saParamString}--${pageNum - 1}`));
@@ -81,8 +81,8 @@ async function cmrSearch (path, params) {
     const saResponse = response.headers['cmr-search-after'];
 
     searchAfterCache.set(quickHash(`${saParamString}--${pageNum}`),
-                         saResponse,
-                         settings.cacheTtl);
+      saResponse,
+      settings.cacheTtl);
   }
   return response;
 }
@@ -269,16 +269,16 @@ async function getGranuleTemporalFacets (params = {}, year, month, day) {
   if (year) {
     // if year provided, get months
     const monthFacet = yearFacet
-          .children.find(y => y.title === year)
-          .children.find(y => y.title === 'Month');
+      .children.find(y => y.title === year)
+      .children.find(y => y.title === 'Month');
     const months = monthFacet.children.map(y => y.title);
     facets.months = months;
     if (month) {
       // if month also provided, get days
       const days = monthFacet
-            .children.find(y => y.title === month)
-            .children.find(y => y.title === 'Day')
-            .children.map(y => y.title);
+        .children.find(y => y.title === month)
+        .children.find(y => y.title === 'Day')
+        .children.map(y => y.title);
       facets.days = days;
     }
     if (day) {
