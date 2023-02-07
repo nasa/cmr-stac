96976e8b07907f8ca351efec12093662e99be635
Jay Barra <jay.barra@nasa.gov>
2023-02-07 16:38:24 -0500

    CMR-8932 comprehensive dateteime validation

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,69 +1,82 @@
-const ISO_8601_DATE_RX = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})[+-](\d{2}):(\d{2})$/;
-const RFC_3339_RX = /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:\d{2})?)?$/;
+const ISO_8601_DATE_RX =
+  /^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d[\,.]\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/i;
 
-const splitOnDelimiters = (str: string, delimiters: Array<string>) => {
-    let splits: any;
-  
-    delimiters.forEach((d) => {
-      if (!splits && str.indexOf(d) !== -1) {
-        splits = str.split(d);
-      }
-    });
-  
-    if (!splits) {
-      splits = [str];
+const RFC_3339_RX = /^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(\.\d+)Z$/i;
+
+const dateOnlyRx = /^\d{4}-\d{2}-\d{2}$/;
+
+const validTimeZoneRx = /[+-]?(\d{2}:\d{2})$|Z$/i;
+
+const splitOnDelimiters = (str: string, delimiters: string[]) => {
+  const splits = delimiters
+    .filter((delimiter) => str.indexOf(delimiter) !== -1)
+    .flatMap((delimiter) => str.split(delimiter));
+
+  if (!splits.length) return [str];
+  return splits;
+};
+
+const isValidDate = (date: string) => {
+  if (date === "" || date === "..") return true;
+
+  if (dateOnlyRx.test(date)) return false;
+  if (!validTimeZoneRx.test(date)) return false;
+  if (!(ISO_8601_DATE_RX.test(date) || RFC_3339_RX.test(date))) return false;
+
+  return !Number.isNaN(new Date(date).getTime());
+};
+
+export const convertDateTime = (dateTime?: string) => {
+  if (!dateTime) return;
+
+  const dateTimeArray = splitOnDelimiters(dateTime, [",", "/"]);
+
+  if (dateTimeArray.length === 1) {
+    if (
+      dateTimeArray[0].substring(
+        dateTimeArray[0].length - 2,
+        dateTimeArray[0].length
+      ) === ".."
+    ) {
+      return dateTimeArray[0].substring(0, dateTimeArray[0].length - 2);
+    } else {
+      return `${dateTimeArray[0]}/${dateTimeArray[0]}`;
     }
-    return splits;
-}
+  }
 
-const isValidDateFormat = (dates: Array<string>) => {
-    if(dates.length > 2) {
-        return false;
+  if (dateTimeArray.length === 2) {
+    if (dateTimeArray[0] === "..") {
+      return `0000-12-31T00:00:00.00Z/${dateTimeArray[1]}`;
     }
-    let isValid = true;
-    dates.forEach(date => {
-        if(!(RFC_3339_RX.test(date) || ISO_8601_DATE_RX.test(date) || date === '..')) {
-            isValid = false;
-        }
-    });
-    return isValid;
-}
-
-export const convertDateTime = (dateTime?: any) => {
-    if(!dateTime) {
-        return "";
+    if (dateTimeArray[1] === "..") {
+      return `${dateTimeArray[0]}/9999-12-31T23:59:59.999Z`;
     }
-    const dateTimeString = dateTime;
-    const dateTimeArray = splitOnDelimiters(dateTimeString, [',', '/']);
+  }
 
-    if(dateTimeArray.length === 1) {
-        if(dateTimeArray[0].substring(dateTimeArray[0].length - 2, dateTimeArray[0].length) === '..') {
-            return dateTimeArray[0].substring(0, dateTimeArray[0].length - 2);
-        } else {
-            return `${dateTimeArray[0]}/${dateTimeArray[0]}`;
-        }
-    }
+  return dateTime;
+};
 
-    if(dateTimeArray.length === 2) {
-        if(dateTimeArray[0] === '..') {
-            return `0000-12-31/${dateTimeArray[1]}`;
-        }
-        if(dateTimeArray[1] === '..') {
-            return `${dateTimeArray[0]}/9999-12-31`;
-        }
-    }
+const invalidUnboundRanges = ["/", "../..", "/..", "../"];
 
-    return dateTimeString;
-}
+const isValidRange = (dates: string[]) => {
+  const [start, end] = dates;
+
+  if (!start || start === ".." || start === "") return true;
+  if (!end || end === ".." || end === "") return true;
+
+  return Number(new Date(end)) - Number(new Date(start)) > 0;
+};
 
 export const validDateTime = (dateTimeString?: string) => {
-    if(!dateTimeString) {
-        return true;
-    }
-    const dates = splitOnDelimiters(dateTimeString, [',', '/']);
-    if(!isValidDateFormat(dates)) {
-        return false;
-    }
-    return true;
-}
+  if (!dateTimeString) return;
+  if (invalidUnboundRanges.find((invalid) => invalid === dateTimeString))
+    return false;
 
+  const dates = splitOnDelimiters(dateTimeString, [",", "/"]);
+  if (dates.length > 2) return false;
+
+  return (
+    dates.reduce((validAcc, d) => validAcc && isValidDate(d), true) &&
+    isValidRange(dates)
+  );
+};
