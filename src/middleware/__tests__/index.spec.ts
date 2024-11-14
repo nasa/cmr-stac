495295037df8feec2560003153b1472b0ec2c00e
william-valencia <51178394+william-valencia@users.noreply.github.com>
2024-11-14 16:04:50 -0500

    CMR-10146: Making sure validBbox compares numbers and not strings (#378)
    * CMR-10146: Making sure validBbox compares numbers and not strings
    
    * CMR-10146: Adding comments
    
    * CMR-10146: Adjusting the bbox types and adding tests
    
    * CMR-10146: Making test prettier

---
+++ src/middleware/__tests__/index.spec.ts
@@ -0,0 +1,34 @@
+import chai from "chai";
+const { expect } = chai;
+
+import { validBbox } from "../index";
+
+describe("validBBOX", () => {
+  describe("when bbox is a string", () => {
+    it("returns a valid bbox", async () => {
+      const bbox = "-122.09,39.89,-122.03,39.92";
+      expect(validBbox(bbox)).to.equal(true);
+    });
+  });
+
+  describe("when bbox is a string array", () => {
+    it("returns a valid bbox", async () => {
+      const bbox = ["-122.09", "39.89", "-122.03", "39.92"];
+      expect(validBbox(bbox)).to.equal(true);
+    });
+  });
+
+  describe("when bbox is an invalid string array with negative numbers", () => {
+    it("returns an invalid bbox", async () => {
+      const bbox = ["-122.03", "39.89", "-122.09", "39.92"];
+      expect(validBbox(bbox)).to.equal(false);
+    });
+  });
+
+  describe("when bbox is a number array", () => {
+    it("returns a valid bbox", async () => {
+      const bbox = [-122.09, 39.89, -122.03, 39.92];
+      expect(validBbox(bbox)).to.equal(true);
+    });
+  });
+});
