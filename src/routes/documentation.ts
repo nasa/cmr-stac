38da008233059e4e19c29fe2e55f199b5e932ef3
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-12-03 11:21:22 -0500

    CMR-10210: fix prettier

--- src/routes/documentation.ts
+++ src/routes/documentation.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import * as path from 'path';
+import * as path from "path";
 
 export const documentationHandler = async (_req: Request, res: Response) => {
-  res.sendFile(path.resolve('docs/index/index.html'));
+  res.sendFile(path.resolve("docs/index/index.html"));
 };
