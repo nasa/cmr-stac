64fb89406012cc2d29f62acec878885f8206bd28
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-12-03 11:41:16 -0500

    CMR-10210: Add log for serverside debugging

--- src/routes/documentation.ts
+++ src/routes/documentation.ts
@@ -2,5 +2,6 @@ import { Request, Response } from "express";
 import * as path from "path";
 
 export const documentationHandler = async (_req: Request, res: Response) => {
+  console.log('🚀 ~ file: documentation.ts:6 ~ path.resolve("docs/index/index.html"):', path.resolve("docs/index/index.html"))
   res.sendFile(path.resolve("docs/index/index.html"));
 };
