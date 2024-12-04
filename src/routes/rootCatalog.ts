840475f8d610d5d027df01e74ccfa9a7fb83979a
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-12-04 13:53:42 -0500

    CMR-10210: Update the type field for the rootcatalog service-desc response

--- src/routes/rootCatalog.ts
+++ src/routes/rootCatalog.ts
@@ -27,7 +27,7 @@ const selfLinks = (req: Request): Link[] => {
       rel: "service-desc",
       href: `https://api.stacspec.org/v1.0.0-beta.1/openapi.yaml`,
       title: "OpenAI Documentation",
-      type: "application/vnd.oai.openapi+json;version=3.0",
+      type: "application/vnd.oai.openapi;version=3.0",
     },
     {
       rel: "service-doc",
