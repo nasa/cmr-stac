3e7b63ad04379dfa5633aae1d78c3ce45ac2a142
KaseyW31 <kasey.wei121@gmail.com>
2025-06-06 16:10:19 -0400

    remove item search conformance for ALL provider

--- src/routes/providerConformance.ts
+++ src/routes/providerConformance.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
-import { conformance } from "../domains/providers";
+import { ALL_PROVIDER, conformance, conformanceAll } from "../domains/providers";
 
 export const providerConformanceHandler = async (_req: Request, res: Response): Promise<void> => {
-  res.json({ conformsTo: conformance });
+  const {
+    params: { providerId: provider },
+  } = _req;
+  res.json({ conformsTo: provider === ALL_PROVIDER ? conformanceAll : conformance });
 };
