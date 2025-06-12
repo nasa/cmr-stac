3f76c62650b2878318edffc9cfe798e723a5e994
KaseyW31 <kasey.wei121@gmail.com>
2025-06-12 10:22:38 -0400

    add unit tests

---
+++ src/__tests__/providerConformance.spec.ts
@@ -0,0 +1,45 @@
+import * as sinon from "sinon";
+import { expect } from "chai";
+import request from "supertest";
+
+import { createApp } from "../app";
+import * as Provider from "../domains/providers";
+import { conformance } from "../domains/providers";
+
+const stacApp = createApp();
+const sandbox = sinon.createSandbox();
+
+describe("GET /:provider/conformance", () => {
+    beforeEach(() => {
+      sandbox
+        .stub(Provider, "getProviders")
+        .resolves([null, [{ "provider-id": "TEST", "short-name": "TEST" }]]);
+    });
+    afterEach(() => {
+        sandbox.restore();
+    })
+
+    describe("given a valid provider", () => {
+        it("should return the correct conformance classes", async () => {
+            const { body, statusCode } = await request(stacApp).get("/stac/TEST/conformance");
+            expect(statusCode).to.equal(200);
+            expect(body.conformsTo).to.deep.equal(conformance);
+        })
+    })
+    describe("given the ALL provider/catalog", () => {
+        it("should have collection search conformance classes", async () => {
+            const { body, statusCode } = await request(stacApp).get("/stac/ALL/conformance");
+            expect(statusCode).to.equal(200);
+            const conformanceClasses = body.conformsTo;
+            const collectionSearchClasses = conformanceClasses.filter((c: String) => c.includes("collection-search"));
+            expect(collectionSearchClasses).not.to.be.empty;
+        })
+        it("should not contain item search conformance classes", async () => {
+            const { body, statusCode } = await request(stacApp).get("/stac/ALL/conformance");
+            expect(statusCode).to.equal(200);
+            const conformanceClasses = body.conformsTo;
+            const itemSearchClasses = conformanceClasses.filter((c: String) => c.includes("item-search"));
+            expect(itemSearchClasses).to.be.empty;
+        })
+    })
+})
