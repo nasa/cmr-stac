9fc30c567131a4b290bd95487635b7edb5ed2e65
EthanGrants <105686621+EthanGrants@users.noreply.github.com>
2025-06-30 07:40:17 -0400

    CMR-7648: Add storage extension (#397)
    * Add storage extension
    
    * Remove importing StacExtension

--- src/domains/__tests__/collections.spec.ts
+++ src/domains/__tests__/collections.spec.ts
@@ -164,11 +164,13 @@ describe("collectionsToStac", () => {
             href: "s3://op-test-protected/SOME_DATA.002",
             title: "op_test_protected_SOME_DATA_002",
             roles: ["data"],
+            "storage:refs": ["aws"],
           },
           s3_op_test_public_SOME_DATA_V003: {
             href: "s3://op-test-public/SOME_DATA.V003",
             title: "op_test_public_SOME_DATA_V003",
             roles: ["data"],
+            "storage:refs": ["aws"],
           },
           s3_credentials: {
             href: "https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials",
@@ -205,17 +207,18 @@ describe("collectionsToStac", () => {
         };
 
         const stacCollection: any = collectionToStac(base);
-
         expect(stacCollection).to.have.deep.property("assets", {
           s3_op_test_protected_SOME_DATA_002: {
             href: "s3://op-test-protected/SOME_DATA.002",
             title: "op_test_protected_SOME_DATA_002",
             roles: ["data"],
+            "storage:refs": ["aws"],
           },
           s3_op_test_public_SOME_DATA_V003: {
             href: "s3://op-test-public/SOME_DATA.V003",
             title: "op_test_public_SOME_DATA_V003",
             roles: ["data"],
+            "storage:refs": ["aws"],
           },
           s3_credentials: {
             href: "https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials",
@@ -232,6 +235,71 @@ describe("collectionsToStac", () => {
             roles: ["metadata"],
             title: "CMR XML metadata for C00000000-TEST_PROV",
             type: "application/xml",
+          },
+        });
+        expect(stacCollection).to.have.deep.property("storage:schemes", {
+          aws: {
+            type: "aws-s3",
+            platform: "https://{bucket}.s3.{region}.amazonaws.com",
+            bucket: "op-test-protected",
+            region: "us-west-2",
+          },
+        });
+      });
+    });
+    describe("given multiple S3 links with public and then protected buckets", () => {
+      it("should return a STAC collection with storage:refs preferring the protected bucket", () => {
+        const [base] = generateCollections(1);
+        base.directDistributionInformation = {
+          region: "us-west-2",
+          s3BucketAndObjectPrefixNames: [
+            "s3://op-test-public/SOME_DATA.V003",
+            "s3://op-test-protected/SOME_DATA.002",
+          ],
+          s3CredentialsApiEndpoint: "https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials",
+          s3CredentialsApiDocumentationUrl:
+            "https://data.lpdaac.earthdatacloud.nasa.gov/s3credentialsREADME",
+        };
+
+        const stacCollection: any = collectionToStac(base);
+
+        expect(stacCollection).to.have.deep.property("assets", {
+          s3_op_test_protected_SOME_DATA_002: {
+            href: "s3://op-test-protected/SOME_DATA.002",
+            title: "op_test_protected_SOME_DATA_002",
+            roles: ["data"],
+            "storage:refs": ["aws"],
+          },
+          s3_op_test_public_SOME_DATA_V003: {
+            href: "s3://op-test-public/SOME_DATA.V003",
+            title: "op_test_public_SOME_DATA_V003",
+            roles: ["data"],
+            "storage:refs": ["aws"],
+          },
+          s3_credentials: {
+            href: "https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials",
+            roles: ["metadata"],
+            title: "S3 credentials API endpoint",
+          },
+          s3_credentials_documentation: {
+            href: "https://data.lpdaac.earthdatacloud.nasa.gov/s3credentialsREADME",
+            roles: ["metadata"],
+            title: "S3 credentials API endpoint documentation",
+          },
+          metadata: {
+            href: "undefined/search/concepts/C00000000-TEST_PROV.xml",
+            roles: ["metadata"],
+            title: "CMR XML metadata for C00000000-TEST_PROV",
+            type: "application/xml",
+          },
+        });
+
+        expect(stacCollection).to.have.deep.property("storage:schemes", {
+          aws: {
+            type: "aws-s3",
+            platform: "https://{bucket}.s3.{region}.amazonaws.com",
+            bucket: "op-test-protected", // select protected even if public bucket comes first
+            region: "us-west-2",
           },
         });
       });
